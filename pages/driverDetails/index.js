5d945ba97d342205e3325a0242f4cc554f5f56b3
chenxin <2582526377@qq.com>
2020-09-21 17:51:31 +0800

    修改bug

--- pages/driverDetails/index.js
+++ pages/driverDetails/index.js
@@ -9,7 +9,7 @@ Page({
     order: {},
     array1: ['电梯', '楼梯'],
     array2: ['低于30米', '30-50米', '50-100米', '100米以上', '地下室出入'],
-    signText:false
+    signText: false
   },
 
   /**
@@ -41,18 +41,18 @@ Page({
       }
     })
   },
-    //提示司机未签到
- botSign(){
-   let _this=this
-  let order=_this.data.order
-   if(order.orderStatus==='待开工'){
-    wx.showToast({
-      title: '检测到你还未签到，请点击签到',
-      icon: 'none',
-      duration: 2000
-    });
-   }
- },
+  //提示司机未签到
+  botSign() {
+    let _this = this
+    let order = _this.data.order
+    if (order.orderStatus === '待开工') {
+      wx.showToast({
+        title: '检测到你还未签到，请点击签到',
+        icon: 'none',
+        duration: 2000
+      });
+    }
+  },
   //点击签到
   clickSign() {
     let that = this;
@@ -60,15 +60,18 @@ Page({
       type: 'wgs84',
       success(res) {
         let from = that.data.order.routes[0].location;
-        let url = 'https://apis.map.qq.com/ws/direction/v1/driving/?from=' + res.latitude + ',' + res.longitude + '&to=' + from.lat + ',' + from.lng + '&output=json&key=OI7BZ-EGOWU-H5YVZ-4HLVW-MDUUQ-ZCFGJ';
-        wx.request({
-          url: url,
-          method: "GET",
-          success: res => {
-            let distance = res.data.result.routes[0].distance;
-            if (distance > 100) {
+        let distance = Number(that.juli(from.lat, from.lng, res.latitude, res.longitude));
+        if (distance > 1) {
+          wx.showToast({
+            title: '请到起点附近签到',
+            icon: 'none',
+            duration: 2000
+          });
+        } else {
+          App._post_form('user/order/signIn/' + that.data.order.id, {}, result => {
+            if (result.code === 1) {
               wx.showToast({
-                title: '请到起点附近签到',
+                title: '签到成功',
                 icon: 'none',
                 duration: 2000
               });
@@ -88,8 +91,8 @@ Page({
                 }
               });
             }
-          }
-        });
+          });
+        }
       }
     })
   },
@@ -105,7 +108,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  this.botSign()
+    this.botSign()
   },
 
   /**
@@ -141,5 +144,16 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  // 计算两地之间的距离
+  juli: function (lat1, lng1, lat2, lng2) {
+    var radLat1 = lat1 * Math.PI / 180.0;
+    var radLat2 = lat2 * Math.PI / 180.0;
+    var a = radLat1 - radLat2;
+    var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+    s = s * 6378.137;
+    s = Math.round(s * 10000) / 10000;
+    return s;
   }
 })
