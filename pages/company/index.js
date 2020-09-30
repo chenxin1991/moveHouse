0de0e3b90d4273ce8ce88a9f31edf8398e0ad4aa
chenxin <2582526377@qq.com>
2020-09-30 11:09:47 +0800

    企业搬迁

--- pages/company/index.js
+++ pages/company/index.js
@@ -1,11 +1,4 @@
-// import {
-//   confirm,
-//   alert,
-//   ajax,
-//   show,
-//   hide,
-//   toLogin,
-// } from '../../../utils/util.js';
+
 import {showToast} from '../../utils/util.js' 
 var reg = /^1[3456789]\d{9}$/
 const app = getApp();
@@ -20,43 +13,7 @@ Page({
   onLoad: function(options) {
 
   },
-  callPhone(e) {
-    wx.makePhoneCall({
-      phoneNumber: e.currentTarget.dataset.phone,
-    })
-  },
-  getInfo(detail) {
-    this.setData({
-      shouquan: false
-    })
-    console.log(detail)
-    show("请稍等!");
-    if (!app.globalData.open_id) {
-      toLogin(app, res => {
-        this.setData({
-          shouquan: false,
-        })
-        hide();
-        this.submit();
-      })
-    } else {
-
-    }
-    hide();
-  },
-  cancel() {
-    this.setData({
-      shouquan: false,
-    })
-  },
   submit() {
-    //判断是否授权
-    // if (!app.globalData.open_id) {
-    //   this.setData({
-    //     shouquan: true
-    //   })
-    //   return;
-    // }
    
     if (!this.data.submitData.firm_name) return showToast('请输入单位名称');
     if (!this.data.submitData.name) return showToast('请输入联系人');    
@@ -64,19 +21,12 @@ Page({
     if (!reg.test(this.data.submitData.phone)) return showToast('请输入正确的联系电话');
     if (!this.data.submitData.remark) return showToast('请输入需求描述');
     let o = this.data.submitData;
-    ajax('/Index/User/unit_move', {
-      contacts: o.name,
-      firm_name: o.firm_name,
-      tel: o.phone,
-      des: o.remark,
-      open_id:app.globalData.open_id
-    }, res => {
-      showToast('添加成功', '', res => {
-        wx.navigateBack({
-          delta: 1,
-        })
-      })
-    },'GET')
+    //   App._get('driver/order/payUrl/' + e.currentTarget.dataset.id, {}, function (result) {
+    //   let code_url=encodeURIComponent(result.data.code_url)
+    //         wx.navigateTo({
+    //           url: '/pages/paymentCode/index?code_url=' + `${code_url}`,
+    //         });
+    //       });
   },
   getValue(e) {
     let label = e.target.id;
@@ -89,14 +39,6 @@ Page({
     })
 
   },
-  call() {
-    confirm('', '是否拨打400-700-8942', res => {
-      wx.makePhoneCall({
-        phoneNumber: '400-700-8942',
-      })
-    })
-  },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
