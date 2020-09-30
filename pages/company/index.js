c8c71484ede77ea4d05eb3dadb11d970aad3ce3f
chenxin <2582526377@qq.com>
2020-09-30 15:04:52 +0800

    单位搬迁

--- pages/company/index.js
+++ pages/company/index.js
@@ -1,90 +1,96 @@
-
-import {showToast} from '../../utils/util.js' 
+import {
+  showToast
+} from '../../utils/util.js'
 var reg = /^1[3456789]\d{9}$/
 const app = getApp();
 Page({
   data: {
-    submitData: {},
-    shouquan: false,
+    submitData: {}
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
 
   },
   submit() {
-   
-    if (!this.data.submitData.firm_name) return showToast('请输入单位名称');
-    if (!this.data.submitData.name) return showToast('请输入联系人');    
+    let userMobile = wx.getStorageSync('mobile');
+    if (!this.data.submitData.name) return showToast('请输入单位名称');
+    if (!this.data.submitData.customer) return showToast('请输入联系人');
     if (!this.data.submitData.phone) return showToast('请输入联系电话');
     if (!reg.test(this.data.submitData.phone)) return showToast('请输入正确的联系电话');
-    if (!this.data.submitData.remark) return showToast('请输入需求描述');
-    let o = this.data.submitData;
-    //   App._get('driver/order/payUrl/' + e.currentTarget.dataset.id, {}, function (result) {
-    //   let code_url=encodeURIComponent(result.data.code_url)
-    //         wx.navigateTo({
-    //           url: '/pages/paymentCode/index?code_url=' + `${code_url}`,
-    //         });
-    //       });
+    if (!this.data.submitData.description) return showToast('请输入需求描述');
+    this.data.submitData.userMobile = userMobile;
+    app._post_form('CompanyOrder/add', this.data.submitData, result => {
+      if (result.code === 1) {
+        wx.showToast({
+          title: '提交成功',
+          icon: 'success',
+          duration: 2000,
+          complete: function () {
+            setTimeout(function () {
+              wx.navigateBack();
+            }, 2000);
+          }
+        })
+      }
+    });
   },
   getValue(e) {
     let label = e.target.id;
     let value = e.detail.value;
     let obj = this.data.submitData;
-    console.log(value)
     obj[label] = value;
     this.setData({
       submitData: obj
-    })
-
+    });
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
